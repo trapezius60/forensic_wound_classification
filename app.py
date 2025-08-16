6f9a862bdf6302a60435dadda2e14f23eac2e93b
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2025-08-16 15:45:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 st.set_page_config(page_title="Wound Detection App", layout="wide")
 
 # ------------------- Header -------------------
-st.title("🩹 Wound Detection YOLOv8")
+st.title("🩹 Forensic Wound Detection YOLOv8")
 st.write("Upload an image or use your webcam for live detection")
 
 # ------------------- Load Model -------------------
@@ -81,3 +81,4 @@ Version: 1.0.0 | Updated: August 2025 | Powered by BH <br>
 <a href='https://docs.google.com/document/d/18KlYv7Xbp3Y4Snatfez_jff0OW7DWKPoYP3HA3fx2cQ/edit?usp=sharing' target='_blank'>📄 User Manual</a>
 </div>
 """, unsafe_allow_html=True)
+
